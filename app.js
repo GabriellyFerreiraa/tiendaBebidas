0f1798d293e2bcc2fe53fdfe4ac17b30e7232f83
Gabrielly Ferreira <gabiferreira@hotmail.com>
2023-05-20 19:00:15 -0300

    3 entrega

--- app.js
+++ app.js
@@ -1,70 +1,151 @@
-var carrito = [];
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData()
+})
 
-function Product(nombre, precio) {
-  this.nombre = nombre;
-  this.precio = precio;
-}
+const fetchData = async () => {
+  try {
+    const res = await fetch('api.json')
+    const data = await res.json()
 
-function displayCarritoCompra() {
-  var carritoString = "Carrito :\n";
-
-  for (var i = 0; i < carrito.length; i++) {
-    carritoString += (i + 1) + ". " + carrito[i].nombre + " - $" + carrito[i].precio.toFixed(2) + "\n";
-  }
-
-  if (carrito.length === 0) {
-    carritoString += "Tu carrito esta vacio.";
-  }
-
-  alert(carritoString);
-}
-
-function addCarrito() {
-  var productoNombre = prompt("Ingrese el nombre del producto: ");
-  var productoPrecio = parseFloat(prompt("Ingrese el precio del producto: "));
-
-  var product = new Product(productoNombre, productoPrecio);
-
-  carrito.push(product);
-
-  alert("Producto agregado al carrito.");
-}
-
-function removerCarrito() {
-  var productoIndex = parseInt(prompt("Ingrese la posicion del producto que queres remover: ")) - 1;
-
-  if (productoIndex >= 0 && productoIndex < carrito.length) {
-    carrito.splice(productoIndex, 1);
-    alert("Producto removido del carrito.");
-  } else {
-    alert("Numero del producto no válido.");
+    pintarProductos(data)
+    detectarBotones(data)
+  } catch (error) {
+    console.log(error)
   }
 }
 
-function calculateTotalPrecio() {
-  var totalPrecio = 0;
+const contendorProductos = document.querySelector('#contenedor-productos')
+const pintarProductos = (data) => {
+  const template = document.querySelector('#template-productos').content
+  const fragment = document.createDocumentFragment()
 
-  for (var i = 0; i < carrito.length; i++) {
-    totalPrecio += carrito[i].precio;
-  }
+  data.forEach(producto => {
 
-  alert("Total price: $" + totalPrecio.toFixed(2));
+    template.querySelector('img').setAttribute('src', producto.thumbnailUrl)
+    template.querySelector('h5').textContent = producto.title
+    template.querySelector('p span').textContent = producto.precio
+    template.querySelector('button').dataset.id = producto.id
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+  })
+  contendorProductos.appendChild(fragment)
 }
 
-while (true) {
-  var choice = prompt("Elige una opcion:\n1. Add el producto en el carrito\n2. Remover el producto del carrito\n3. Ver productos del carrito\n4. Calcular precio total\n5. Exit");
+let carrito = {}
 
-  if (choice === "1") {
-    addCarrito();
-  } else if (choice === "2") {
-    removerCarrito();
-  } else if (choice === "3") {
-    displayCarritoCompra();
-  } else if (choice === "4") {
-    calculateTotalPrecio();
-  } else if (choice === "5") {
-    break;
-  } else {
-    alert("Elección no válida. Inténtalo de nuevo.");
+const detectarBotones = (data) => {
+  const botones = document.querySelectorAll('.card button')
+
+  botones.forEach(btn => {
+    btn.addEventListener('click', () => {
+
+      const producto = data.find(item => item.id === parseInt(btn.dataset.id))
+      producto.cantidad = 1
+      if (carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad + 1
+      }
+      carrito[producto.id] = { ...producto }
+
+      pintarCarrito()
+    })
+  })
+}
+
+const items = document.querySelector('#items')
+
+const pintarCarrito = () => {
+
+  items.innerHTML = ''
+
+  const template = document.querySelector('#template-carrito').content
+  const fragment = document.createDocumentFragment()
+
+  Object.values(carrito).forEach(producto => {
+
+    template.querySelector('th').textContent = producto.id
+    template.querySelectorAll('td')[0].textContent = producto.title
+    template.querySelectorAll('td')[1].textContent = producto.cantidad
+    template.querySelector('span').textContent = producto.precio * producto.cantidad
+
+
+    template.querySelector('.btn-info').dataset.id = producto.id
+    template.querySelector('.btn-danger').dataset.id = producto.id
+
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+  })
+
+  items.appendChild(fragment)
+
+  pintarFooter()
+  accionBotones()
+
+}
+
+const footer = document.querySelector('#footer-carrito')
+const pintarFooter = () => {
+
+  footer.innerHTML = ''
+
+  if (Object.keys(carrito).length === 0) {
+    footer.innerHTML = `
+      <th scope="row" colspan="5">Carrito vacío</th>
+      `
+    return
   }
+
+  const template = document.querySelector('#template-footer').content
+  const fragment = document.createDocumentFragment()
+
+ 
+  const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+  const nPrecio = Object.values(carrito).reduce((acc, { cantidad, precio }) => acc + cantidad * precio, 0)
+ 
+
+  template.querySelectorAll('td')[0].textContent = nCantidad
+  template.querySelector('span').textContent = nPrecio
+
+  const clone = template.cloneNode(true)
+  fragment.appendChild(clone)
+
+  footer.appendChild(fragment)
+
+
+  const boton = document.querySelector('#vaciar-carrito')
+  boton.addEventListener('click', () => {
+    carrito = {}
+    pintarCarrito()
+  })
+
+}
+
+const accionBotones = () => {
+  const botonesAgregar = document.querySelectorAll('#items .btn-info')
+  const botonesEliminar = document.querySelectorAll('#items .btn-danger')
+
+
+
+  botonesAgregar.forEach(btn => {
+    btn.addEventListener('click', () => {
+
+      const producto = carrito[btn.dataset.id]
+      producto.cantidad++
+      carrito[btn.dataset.id] = { ...producto }
+      pintarCarrito()
+    })
+  })
+
+  botonesEliminar.forEach(btn => {
+    btn.addEventListener('click', () => {
+
+      const producto = carrito[btn.dataset.id]
+      producto.cantidad--
+      if (producto.cantidad === 0) {
+        delete carrito[btn.dataset.id]
+      } else {
+        carrito[btn.dataset.id] = { ...producto }
+      }
+      pintarCarrito()
+    })
+  })
 }
